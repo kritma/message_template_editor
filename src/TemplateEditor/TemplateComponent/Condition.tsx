ed3ed048a4ba0a253c4c06859de93982e1d43db2
kritma <silibakaboy@gmail.com>
2023-08-29 16:02:53 +0300

    better animations

--- src/TemplateEditor/TemplateComponent/Condition.tsx
+++ src/TemplateEditor/TemplateComponent/Condition.tsx
@@ -1,16 +1,42 @@
-import { TemplateCondition } from '../../utils/dtoWrappers'
+import { TemplateCondition, TemplateString } from '../../utils/dtoWrappers'
 import { Container } from './Container'
 import styles from './Condition.module.css'
 import { useEditorDispatch } from '../EditorProvider/EditorProvider'
+import { useEffect, useRef, useState } from 'react'
+
+const TRANSITION_DURATION = 200 // ms
 
 export function Condition({ self }: { self: TemplateCondition }) {
     const dispatch = useEditorDispatch()
+    const [height, setHeight] = useState<number | 'auto'>(0)
+    const condition = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        setHeight(condition.current!.clientHeight)
+        setTimeout(() => setHeight('auto'), TRANSITION_DURATION)
+    }, [])
+
     return (
-        <div className={styles.condition_wrapper}>
-            <button className={styles.delete} onClick={() => dispatch({ type: 'delete_condition', id: self.id })}>
+        <div className={styles.condition_wrapper}
+            style={
+                {
+                    transition: `all ${TRANSITION_DURATION}ms linear`,
+                    height: height
+                }
+            }>
+            <button className={styles.delete} onClick={(e) => {
+                // right now it either clientHeight or 'auto' but we need clientHeight bcs css cant animate from/to "auto"
+                setHeight(condition.current!.clientHeight)
+
+                // defer so transition occurs
+                setTimeout(() => setHeight(0), 0)
+
+                // remove after transition
+                setTimeout(() => dispatch({ type: 'delete_condition', id: self.id }), TRANSITION_DURATION)
+            }}>
                 x
             </button>
-            <div className={styles.condition}>
+            <div ref={condition} className={styles.condition}>
                 <div className={styles.branch_container}><p className={styles.branch}>if</p> <Container self={self.condition} /></div>
                 <div className={styles.branch_container}><p className={styles.branch}>then</p> <Container self={self.then} /></div>
                 <div className={styles.branch_container}><p className={styles.branch}>else</p> <Container self={self.otherwise} /></div>
